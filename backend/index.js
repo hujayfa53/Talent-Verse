5a7ea5e768d62006f48b5950a41e4ec573486697
hr1971 <rifathujayfa@gmail.com>
2025-12-08 22:20:42 +0600

    make single contest  api

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const admin = require("firebase-admin");
 const port = process.env.PORT || 3000;
 const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
@@ -73,6 +73,15 @@ async function run() {
 
     app.get('/contests', async (req,res) => {
       const result = await contestsCollection.find().toArray()
+      res.send(result)
+    })
+
+
+    // ---get single contest from db
+
+    app.get('/contests/:id', async (req,res) => {
+      const id =req.params.id
+      const result = await contestsCollection.findOne({_id: new ObjectId(id)})
       res.send(result)
     })
     
